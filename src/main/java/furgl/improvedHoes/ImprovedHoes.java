a81538048b3f6110e403bc881ce6a468c3725051
Furgl <jake.pollard15@gmail.com>
2016-11-08 18:52:08 -0600

    updated

--- src/main/java/furgl/improvedHoes/ImprovedHoes.java
+++ src/main/java/furgl/improvedHoes/ImprovedHoes.java
@@ -13,7 +13,7 @@ public class ImprovedHoes
 	public static final String MODID = "improvedHoes";
 	public static final String MODNAME = "Improved Hoes";
 	public static final String VERSION = "1.1";
-
+ 
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event)
 	{
