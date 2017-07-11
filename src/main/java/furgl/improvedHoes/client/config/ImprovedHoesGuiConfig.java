d7417ef0abc2ba45cd7ce18e20b3c8cde4d83f17
Furgl <jake.pollard15@gmail.com>
2017-07-11 12:30:28 -0500

    port to 1.12 and v1.2.2

---
+++ src/main/java/furgl/improvedHoes/client/config/ImprovedHoesGuiConfig.java
@@ -0,0 +1,26 @@
+package furgl.improvedHoes.client.config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import furgl.improvedHoes.common.ImprovedHoes;
+import furgl.improvedHoes.common.config.Config;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraftforge.common.config.ConfigElement;
+import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.fml.client.config.GuiConfig;
+import net.minecraftforge.fml.client.config.IConfigElement;
+
+public class ImprovedHoesGuiConfig extends GuiConfig 
+{
+	public ImprovedHoesGuiConfig(GuiScreen parent) 
+	{
+		super(parent, getConfigElements(), ImprovedHoes.MODID, false, false, "Improved Hoes Configuration");
+	}
+
+	private static List<IConfigElement> getConfigElements() {
+        final List<IConfigElement> list = new ArrayList<IConfigElement>();
+        list.addAll(new ConfigElement(Config.config.getCategory(Configuration.CATEGORY_GENERAL)).getChildElements());
+        return list;
+    }
+}
--- src/main/java/furgl/improvedHoes/common/ImprovedHoes.java
+++ src/main/java/furgl/improvedHoes/common/ImprovedHoes.java
@@ -11,12 +11,12 @@ import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = ImprovedHoes.MODID, name = ImprovedHoes.MODNAME, version = ImprovedHoes.VERSION, guiFactory = "furgl.improvedHoes.client.config.ImprovedHoesGuiFactory", updateJSON = "https://raw.githubusercontent.com/Furgl/ImprovedHoes/1.11.2/update.json")
+@Mod(modid = ImprovedHoes.MODID, name = ImprovedHoes.MODNAME, version = ImprovedHoes.VERSION, guiFactory = "furgl.improvedHoes.client.config.ImprovedHoesGuiFactory", updateJSON = "https://raw.githubusercontent.com/Furgl/ImprovedHoes/1.12/update.json")
 public class ImprovedHoes
 {
 	public static final String MODID = "improvedhoes";
 	public static final String MODNAME = "Improved Hoes";
-	public static final String VERSION = "1.2.1";
+	public static final String VERSION = "1.2.2";
 
 	@EventHandler 
 	public void preInit(FMLPreInitializationEvent event) {
@@ -41,6 +41,8 @@ public class ImprovedHoes
 	}
 
 	public static int calculateRadius(ItemStack stack) {
+		stack = stack.copy();
+		stack.clearCustomName();
 		int radius = 0;
 		if (Config.radius0Hoes.contains(stack.getDisplayName()))
 			radius = 0;
