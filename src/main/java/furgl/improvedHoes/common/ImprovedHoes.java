3104d8164e733f71d7cd7bca7bec986030b752b9
Furgl <jake.pollard15@gmail.com>
2017-07-11 12:29:59 -0500

    v1.2.2

--- src/main/java/furgl/improvedHoes/common/ImprovedHoes.java
+++ src/main/java/furgl/improvedHoes/common/ImprovedHoes.java
@@ -16,7 +16,7 @@ public class ImprovedHoes
 {
 	public static final String MODID = "improvedhoes";
 	public static final String MODNAME = "Improved Hoes";
-	public static final String VERSION = "1.2.1";
+	public static final String VERSION = "1.2.2";
 
 	@EventHandler 
 	public void preInit(FMLPreInitializationEvent event) {
@@ -41,6 +41,8 @@ public class ImprovedHoes
 	}
 
 	public static int calculateRadius(ItemStack stack) {
+		stack = stack.copy();
+		stack.clearCustomName();
 		int radius = 0;
 		if (Config.radius0Hoes.contains(stack.getDisplayName()))
 			radius = 0;
